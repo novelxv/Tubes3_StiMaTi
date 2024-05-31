872e57486e51262305301d24c241b7d7d610861b
novelxv <novelyaputriramadhani11@gmail.com>
2024-05-31 22:45:17 +0700

    feat: build succeed

--- src/algo/BM.cs
+++ src/algo/BM.cs
@@ -1,78 +1,80 @@
-public static class BoyerMoore {
-    /* Function to match two patterns with Boyer-Moore Algorithm */
-    public static bool IsAMatch(string pattern1, string pattern2){
-        // array untuk menyimpan bad character
-        int[] badChar = new int[256];
-        // buat bad character array
-        MakeBadChar(pattern1, pattern1.Length, ref badChar);
+namespace algo {
+    public static class BoyerMoore {
+        /* Function to match two patterns with Boyer-Moore Algorithm */
+        public static bool IsAMatch(string pattern1, string pattern2){
+            // array untuk menyimpan bad character
+            int[] badChar = new int[256];
+            // buat bad character array
+            MakeBadChar(pattern1, pattern1.Length, ref badChar);
 
-        // array untuk menyimpan good suffix
-        int[] goodSuffix = new int[pattern1.Length];
-        // buat good suffix array
-        MakeGoodSuffix(pattern1, pattern1.Length, ref goodSuffix);
+            // array untuk menyimpan good suffix
+            int[] goodSuffix = new int[pattern1.Length];
+            // buat good suffix array
+            MakeGoodSuffix(pattern1, pattern1.Length, ref goodSuffix);
 
-        int shiftIndex = 0;
-        // iterasi sepanjang pattern2
-        while (shiftIndex <= (pattern2.Length - pattern1.Length)){
-            int i = pattern1.Length - 1;
-            // iterasi sepanjang pattern1
-            while (i >= 0 && pattern1[i] == pattern2[shiftIndex + i]){
-                i--;
-            }
-            // jika pattern1 sudah habis
-            if (i < 0){
-                return true;
-            } 
-            else {
-                // geser pattern1
-                shiftIndex += Math.Max(goodSuffix[i + 1], i - badChar[pattern2[shiftIndex + i]]);
-            }
-        }
-        return false;
-    }
-
-    /* Function to create the bad character array */
-    private static void MakeBadChar(string pattern, int length, ref int[] badChar){
-        // inisialisasi semua karakter dengan -1
-        for (int i = 0; i < 256; i++){
-            badChar[i] = -1;
-        }
-        // isi bad character array dengan indeks terakhir dari karakter
-        for (int i = 0; i < length; i++){
-            badChar[(int)pattern[i]] = i;
-        }
-    }
-
-    /* Function to create the good suffix array */
-    private static void MakeGoodSuffix(string pattern, int length, ref int[] goodSuffix){
-        // array untuk menyimpan nilai border
-        int[] border = new int[length + 1];
-        int i = length;
-        int j = length + 1;
-        border[i] = j;
-
-        // iterasi dari belakang
-        while (i > 0){
-            // jika karakter pattern tidak cocok dengan border
-            while (j <= length && pattern[i - 1] != pattern[j - 1]){
-                if (goodSuffix[j] == 0){
-                    goodSuffix[j] = j - i;
+            int shiftIndex = 0;
+            // iterasi sepanjang pattern2
+            while (shiftIndex <= (pattern2.Length - pattern1.Length)){
+                int i = pattern1.Length - 1;
+                // iterasi sepanjang pattern1
+                while (i >= 0 && pattern1[i] == pattern2[shiftIndex + i]){
+                    i--;
                 }
-                j = border[j]; // geser border
+                // jika pattern1 sudah habis
+                if (i < 0){
+                    return true;
+                } 
+                else {
+                    // geser pattern1
+                    shiftIndex += Math.Max(goodSuffix[i + 1], i - badChar[pattern2[shiftIndex + i]]);
+                }
             }
-            i--;
-            j--;
-            border[i] = j;
+            return false;
         }
 
-        j = border[0];
-        // iterasi dari depan
-        for (i = 0; i <= length; i++){
-            if (goodSuffix[i] == 0){
-                goodSuffix[i] = j;
+        /* Function to create the bad character array */
+        private static void MakeBadChar(string pattern, int length, ref int[] badChar){
+            // inisialisasi semua karakter dengan -1
+            for (int i = 0; i < 256; i++){
+                badChar[i] = -1;
             }
-            if (i == j){
-                j = border[j];
+            // isi bad character array dengan indeks terakhir dari karakter
+            for (int i = 0; i < length; i++){
+                badChar[(int)pattern[i]] = i;
+            }
+        }
+
+        /* Function to create the good suffix array */
+        private static void MakeGoodSuffix(string pattern, int length, ref int[] goodSuffix){
+            // array untuk menyimpan nilai border
+            int[] border = new int[length + 1];
+            int i = length;
+            int j = length + 1;
+            border[i] = j;
+
+            // iterasi dari belakang
+            while (i > 0){
+                // jika karakter pattern tidak cocok dengan border
+                while (j <= length && pattern[i - 1] != pattern[j - 1]){
+                    if (goodSuffix[j] == 0){
+                        goodSuffix[j] = j - i;
+                    }
+                    j = border[j]; // geser border
+                }
+                i--;
+                j--;
+                border[i] = j;
+            }
+
+            j = border[0];
+            // iterasi dari depan
+            for (i = 0; i <= length; i++){
+                if (goodSuffix[i] == 0){
+                    goodSuffix[i] = j;
+                }
+                if (i == j){
+                    j = border[j];
+                }
             }
         }
     }
--- src/algo/KMP.cs
+++ src/algo/KMP.cs
@@ -1,58 +1,61 @@
-public static class KMP {
-    /* Function to match two patterns with KMP Algorithm */
-    public static bool IsAMatch(string pattern1, string pattern2){
-        // array untuk menyimpan longest prefix suffix
-        int[] lpsArray = new int[pattern1.Length];
-        int i, j = 0;
-        // buat lpsArray
-        MakeLPSArray(pattern1, pattern1.Length, lpsArray);
-        // iterasi sepanjang pattern2
-        while (j < pattern2.Length){
-            // jika karakter sama
-            if (pattern1[i] == pattern2[j]){
-                i++;
-                j++;
-            }
-            // jika pattern1 sudah habis
-            if (i == pattern1.Length){
-                return true;
-            }
-            // jika karakter tidak sama
-            else if (j < pattern2.Length && pattern1[i] != pattern2[j]){
-                if (i != 0){
-                    // kembali ke nilai LPS sebelumnya
-                    i = lpsArray[i - 1];
-                } 
-                else {
-                    // geser teks
+namespace algo {
+    public static class KMP {
+        /* Function to match two patterns with KMP Algorithm */
+        public static bool IsAMatch(string pattern1, string pattern2){
+            // array untuk menyimpan longest prefix suffix
+            int[] lpsArray = new int[pattern1.Length];
+            int i = 0;
+            int j = 0;
+            // buat lpsArray
+            MakeLPSArray(pattern1, pattern1.Length, lpsArray);
+            // iterasi sepanjang pattern2
+            while (j < pattern2.Length){
+                // jika karakter sama
+                if (pattern1[i] == pattern2[j]){
+                    i++;
                     j++;
                 }
+                // jika pattern1 sudah habis
+                if (i == pattern1.Length){
+                    return true;
+                }
+                // jika karakter tidak sama
+                else if (j < pattern2.Length && pattern1[i] != pattern2[j]){
+                    if (i != 0){
+                        // kembali ke nilai LPS sebelumnya
+                        i = lpsArray[i - 1];
+                    } 
+                    else {
+                        // geser teks
+                        j++;
+                    }
+                }
             }
+            return false;
         }
-        return false;
-    }
 
-    /* Function to create the lpsArray */
-    private static void MakeLPSArray(string pattern, int length, int[] lpsArray){
-        int prevLength = 0; // panjang prefix suffix sebelumnya
-        lpsArray[0] = 0;
-        int i = 1;
-        // iterasi sepanjang pattern
-        while (i < length){
-            // jika karakter pada pattern cocok dengan karakter prefix
-            if (pattern[i] == pattern[prevLength]){
-                prevLength++;
-                lpsArray[i] = prevLength;
-                i++;
-            } 
-            else {
-                if (prevLength != 0){
-                    // kembali ke nilai LPS sebelumnya
-                    prevLength = lpsArray[prevLength - 1];
+        /* Function to create the lpsArray */
+        private static void MakeLPSArray(string pattern, int length, int[] lpsArray){
+            int prevLength = 0; // panjang prefix suffix sebelumnya
+            lpsArray[0] = 0;
+            int i = 1;
+            // iterasi sepanjang pattern
+            while (i < length){
+                // jika karakter pada pattern cocok dengan karakter prefix
+                if (pattern[i] == pattern[prevLength]){
+                    prevLength++;
+                    lpsArray[i] = prevLength;
+                    i++;
                 } 
                 else {
-                    lpsArray[i] = 0;
-                    i++;
+                    if (prevLength != 0){
+                        // kembali ke nilai LPS sebelumnya
+                        prevLength = lpsArray[prevLength - 1];
+                    } 
+                    else {
+                        lpsArray[i] = 0;
+                        i++;
+                    }
                 }
             }
         }
