a12467507792af2ac8ba3ab2a9c22dcfd2a2ea41
slntkllr01 <raffaelsiahaan@gmail.com>
2024-06-01 18:45:04 +0700

    feat: add database with dummy data

--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993aed7d7910310e5cae91e68a18c73a22b62038")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e74328ac039be1bab28c01d89250766371612f59")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
